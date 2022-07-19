c168da3bc612ef7b3bc063b0f07b35ea9a4bc45c
ChenJunjie <2602087227@qq.com>
2022-07-19 09:22:56 +0800

    spring springmvc first modification

--- src/main/java/com/cjj/controller/UserController.java
+++ src/main/java/com/cjj/controller/UserController.java
@@ -1,26 +1,56 @@
 package com.cjj.controller;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Controller
+//@RequestMapping("/test")
 @RequestMapping("/test")
 public class UserController {
 
-    @RequestMapping("/quick")
+//    @RequestMapping(value = "/quick",method = RequestMethod.GET,params = {"username"})
+    @RequestMapping("/test")
     public String save(){
         System.out.println("Controller save running!");
         return "success";
     }
-
-    @RequestMapping("/test")
-    @ResponseBody
-    public String test(HttpServletRequest request, HttpServletResponse response, String username){
-        System.out.println("Controller test running!");
+    @RequestMapping("/quick5")
+    public String save5(HttpServletRequest request){
+        request.setAttribute("username","jie");
         return "success";
     }
+    @RequestMapping("/quick4")
+    public String save4(Model model){
+        model.addAttribute("username","jun");
+        return "success";
+    }
+
+    @RequestMapping("/quick3")
+    public ModelAndView save3(ModelAndView modelAndView){
+        modelAndView.setViewName("success");
+        modelAndView.addObject("username","chen");
+        return modelAndView;
+    }
+    @RequestMapping("/quick2")
+    public ModelAndView save2(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("username","cjj");
+        modelAndView.setViewName("success");
+        return modelAndView;
+    }
+//
+//    @RequestMapping("/test")
+//    @ResponseBody
+//    public String test(HttpServletRequest request, HttpServletResponse response, String username){
+//        System.out.println("Controller test running!");
+//        return "success";
+//    }
 }
